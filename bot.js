f49aa6c98102a39c90b24140ec6bfd30a14225f6
Noob Gamer <39646826+biswajit9123@users.noreply.github.com>
2018-12-28 13:05:57 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,37 @@ const client = new Client({ disableEveryone: true});
 const youtube = new YouTube(config.GOOGLE_API_KEY);
 const PREFIX = config.prefix;
 
+const invites = {};
+
+const wait = require('util').promisify(setTimeout);
+
+client.on('ready', () => {
+  wait(1000);
+
+  client.guilds.forEach(g => {
+    g.fetchInvites().then(guildInvites => {
+      invites[g.id] = guildInvites;
+    });
+  });
+});
+
+client.on('guildMemberAdd', member => {
+  member.guild.fetchInvites().then(guildInvites => {
+   
+    const ei = invites[member.guild.id];
+  
+    invites[member.guild.id] = guildInvites;
+    
+    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+    
+    const inviter = client.users.get(invite.inviter.id);
+
+    const logChannel = member.guild.channels.find(channel => channel.id === "channel id here");
+   
+    logChannel.send(`<@${member.user.id}> **joined**; Invited By **${inviter.username}** (**${invite.uses}** Invites)`);
+  });
+});
+
 const queue = new Map();
 
 client.on('warn', console.warn);
